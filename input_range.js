64b97cbb0d2620d87e5b99a24b0c0e8a66a8d1ee
Dmitry <fadeev2990@gmail.com>
2020-06-12 20:16:31 +0400

    input range start

---
+++ input_range.js
@@ -0,0 +1,6 @@
+let slider = document.querySelector(".slider");
+
+function range() {
+    let val = slider.value;
+    slider.style.background = 'linear-gradient(90deg,#59f083' + x + '%, #ffffff' + x + '% )';
+}
